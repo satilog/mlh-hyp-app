775066eeaa5156f3376566f5e5d1773d155144a8
KlausMikhaelson <satyamsingh5076@gmail.com>
2024-07-13 17:15:00 -0600

    created a simple landing page in /landing route

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
   env: {
     API_URL: process.env.API_URL,
   },
+  images: {
+    domains: ['images.unsplash.com', 'assets.aceternity.com', 'static.vecteezy.com', 'tctrail.ca', 'c8.alamy.com', 'media.istockphoto.com'],
+  }
 }
 
 module.exports = nextConfig
